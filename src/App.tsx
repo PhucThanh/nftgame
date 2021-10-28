51a35632c93a76255b431ded379f06dfe3a67987
Phuc Thanh Tran <phucttran@kms.com.vn>
2021-10-28 13:31:54 +0700

    add cool tile WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { useMoralis } from "react-moralis";
@@ -5,19 +6,47 @@ import Phaser from "phaser";
 import { IonPhaser, GameInstance } from "@ion-phaser/react";
 
 import MainScene from "./MainScence";
+const gamez = {
+  width: "100%",
+  height: "100%",
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.ENVELOP,
+  },
+  scene: {
+    init: function () {
+      this.cameras.main.setBackgroundColor("#24252A");
+    },
+    create: function () {
+      this.helloWorld = this.add.text(
+        this.cameras.main.centerX,
+        this.cameras.main.centerY,
+        "Hello World",
+        {
+          font: "40px Arial",
+          fill: "#ffffff",
+        }
+      );
+      this.helloWorld.setOrigin(0.5);
+    },
+    update: function () {
+      this.helloWorld.angle += 1;
+    },
+  },
+};
 
 const gameConfig: GameInstance = {
   type: Phaser.AUTO,
+  width: "100%",
+  height: "100%",
   scale: {
-    mode: Phaser.Scale.ScaleModes.FIT,
-    //autoCenter: Phaser.Scale.Center.CENTER_BOTH,
-    width: 1024,
-    height: 400,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.Center.CENTER_BOTH,
   },
   physics: {
     default: "arcade",
     arcade: {
-      debug: false,
+      debug: true,
     },
   },
   render: { antialias: false, pixelArt: true, roundPixels: true },
@@ -30,6 +59,7 @@ function App() {
   const gameRef = useRef<HTMLIonPhaserElement>(null);
   const [game, setGame] = useState<GameInstance>(gameConfig);
   const [initialize, setInitialize] = useState(false);
+  const [width, setWidth] = useState("99%");
 
   useEffect(() => {
     console.log(JSON.stringify(user));
@@ -45,7 +75,11 @@ function App() {
       setGame(Object.assign({}, gameConfig));
     }
   }, [initialize]);
-
+  return (
+    <div id="phaser-container" style={{ height: "100vh", width: { width } }}>
+      <IonPhaser ref={gameRef} game={game} />
+    </div>
+  );
   if (!isAuthenticated) {
     return (
       <div>
@@ -57,33 +91,31 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <h1>Welcome {user?.get("username")}</h1>
       <button onClick={() => logout()} disabled={isAuthenticating}>
         Logout
       </button>
 
-      <header className="App-header">
-        {initialize ? (
-          <>
-            <IonPhaser ref={gameRef} game={game} initialize={initialize} />
-            <div onClick={destroy} className="flex destroyButton">
-              <a href="#1" className="bttn">
-                End game
-              </a>
-            </div>
-          </>
-        ) : (
-          <>
-            <div onClick={() => setInitialize(true)} className="flex">
-              <a href="#1" className="bttn">
-                Start game
-              </a>
-            </div>
-          </>
-        )}
-      </header>
-    </div>
+      {initialize ? (
+        <>
+          <IonPhaser ref={gameRef} game={game} initialize={initialize} />
+          <div onClick={destroy} className="flex destroyButton">
+            <a href="#1" className="bttn">
+              End game
+            </a>
+          </div>
+        </>
+      ) : (
+        <>
+          <div onClick={() => setInitialize(true)} className="flex">
+            <a href="#1" className="bttn">
+              Start game
+            </a>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
--- src/MainScence.tsx
+++ src/MainScence.tsx
@@ -1,5 +1,11 @@
 import Moralis from "moralis";
 import tiles from "./images/tiles.png";
+import grass from "./assets/grass.png";
+import road from "./assets/road.png";
+import buildings from "./assets/buildings.png";
+import village from "./tiles/village.json";
+import playerSprite from "./assets/player.png";
+
 class MainScene extends Phaser.Scene {
   private wKey!: Phaser.Input.Keyboard.Key;
   private aKey!: Phaser.Input.Keyboard.Key;
@@ -7,37 +13,119 @@ class MainScene extends Phaser.Scene {
   private dKey!: Phaser.Input.Keyboard.Key;
   private sprites: any = {};
   private state: any = {};
-  private keyLock = true;
-  private wallLayer: any;
+  private wallLayer!: Phaser.Tilemaps.TilemapLayer;
   private treeLayer: any;
   private currentId!: string;
   private timer = 0;
+  private road!: any;
 
   init() {
     this.cameras.main.setBackgroundColor("#24252A");
   }
   preload() {
     this.ping();
+    this.load.image("grass", grass);
+    this.load.image("road", road);
+    this.load.image("buildings", buildings);
     this.load.image("tiles", tiles);
-    this.load.tilemapTiledJSON("map", `${process.env.PUBLIC_URL}/tiles.json`);
+    // this.load.tilemapTiledJSON("map", `${process.env.PUBLIC_URL}/tiles.json`);
+    this.load.tilemapTiledJSON("map", village);
   }
   async create() {
+    this.currentId = Moralis.User.current()?.get("username");
+    this.cameras.main.zoom = 0.4;
+
+    //@ts-ignore
+    document.getElementById("phaser-container").style.width = "99%";
+
     this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-    const map = this.make.tilemap({ key: "map" });
-    const tileset = map.addTilesetImage("normal", "tiles");
+    const map = this.make.tilemap({
+      key: "map",
+      tileWidth: 256,
+      tileHeight: 256,
+    });
+    const grassTileset = map.addTilesetImage("grass", "grass", 256, 256);
+    const roadTileset = map.addTilesetImage("road", "road", 256, 256);
+    const buildingTileset = map.addTilesetImage(
+      "buildings",
+      "buildings",
+      843,
+      874
+    );
 
-    this.wallLayer = map.createLayer("Wall", tileset).setDepth(-1);
-    this.treeLayer = map.createLayer("Trees", tileset).setDepth(-2);
-    const groundLayer = map.createLayer("Ground", tileset).setDepth(-3);
+    const land = map.createLayer("Land", grassTileset).setDepth(-3);
+    this.road = map.createLayer("Road", roadTileset).setDepth(-2);
 
-    this.wallLayer.setCollisionByProperty({ collide: true });
-    this.treeLayer.setCollisionByProperty({ collide: true });
+    this.wallLayer = map.createLayer("Building", buildingTileset);
+    this.wallLayer.setCollisionFromCollisionGroup();
 
-    this.currentId = Moralis.User.current()?.get("username");
+    var graphics = this.add.graphics();
+    this.wallLayer.forEachTile((tile) => {
+      var tileWorldPos = this.wallLayer.tileToWorldXY(tile.x, tile.y);
+      var collisionGroup = buildingTileset.getTileCollisionGroup(tile.index);
+      console.log(buildingTileset);
+      //@ts-ignore
+      if (!collisionGroup || collisionGroup.objects.length === 0) {
+        return;
+      }
+
+      // You can assign custom properties to the whole collision object layer (or even to
+      // individual objects within the layer). Here, use a custom property to change the color of
+      // the stroke.
+      //@ts-ignore
+      if (
+        //@ts-ignore
+        collisionGroup.properties &&
+        //@ts-ignore
+        collisionGroup.properties.isInteractive
+      ) {
+        graphics.lineStyle(5, 0x00ff00, 1);
+      } else {
+        graphics.lineStyle(5, 0x00ffff, 1);
+      }
+
+      // The group will have an array of objects - these are the individual collision shapes
+      //@ts-ignore
+      var objects = collisionGroup.objects;
+
+      for (var i = 0; i < objects.length; i++) {
+        var object = objects[i];
+        var objectX = tileWorldPos.x + object.x;
+        var objectY = tileWorldPos.y + object.y;
+
+        // When objects are parsed by Phaser, they will be guaranteed to have one of the
+        // following properties if they are a rectangle/ellipse/polygon/polyline.
+        if (object.rectangle) {
+          graphics.strokeRect(objectX, objectY, object.width, object.height);
+        } else if (object.ellipse) {
+          // Ellipses in Tiled have a top-left origin, while ellipses in Phaser have a center
+          // origin
+          graphics.strokeEllipse(
+            objectX + object.width / 2,
+            objectY + object.height / 2,
+            object.width,
+            object.height
+          );
+        } else if (object.polygon || object.polyline) {
+          var originalPoints = object.polygon
+            ? object.polygon
+            : object.polyline;
+          var points = [];
+          for (var j = 0; j < originalPoints.length; j++) {
+            var point = originalPoints[j];
+            points.push({
+              x: objectX + point.x,
+              y: objectY + point.y,
+            });
+          }
+          graphics.strokePoints(points);
+        }
+      }
+    });
 
     let query = new Moralis.Query("GameState");
     let subscription = await query.subscribe();
@@ -52,14 +140,13 @@ class MainScene extends Phaser.Scene {
   async update(time: any, delta: any) {
     this.timer++;
     if (this.timer > 10 && this.sprites[this.currentId]) {
-      console.log("move");
-      Moralis.Cloud.run("move", {
-        direction: "right",
-        position: {
-          x: this.sprites[this.currentId].x,
-          y: this.sprites[this.currentId].y,
-        },
-      });
+      // Moralis.Cloud.run("move", {
+      //   direction: "right",
+      //   position: {
+      //     x: this.sprites[this.currentId].x,
+      //     y: this.sprites[this.currentId].y,
+      //   },
+      // });
       this.timer = 0;
     }
 
@@ -71,30 +158,38 @@ class MainScene extends Phaser.Scene {
       this.sprites[this.currentId].setVelocityY(0);
     }
 
-    if (this.wKey.isDown && this.keyLock) {
-      this.keyLock = true;
-      this.sprites[this.currentId].setVelocityY(-200);
-      this.sprites[this.currentId].play("up");
-      this.keyLock = true;
-    }
-    if (this.aKey.isDown && this.keyLock) {
-      this.keyLock = false;
-      this.sprites[this.currentId].setVelocityX(-200);
-      this.sprites[this.currentId].play("left");
-      this.keyLock = true;
-    }
-    if (this.sKey.isDown && this.keyLock) {
-      this.keyLock = false;
-      this.sprites[this.currentId].setVelocityY(200);
-      this.sprites[this.currentId].play("down");
-      this.keyLock = true;
-    }
-    if (this.dKey.isDown && this.keyLock) {
-      this.keyLock = false;
-
-      this.sprites[this.currentId].setVelocityX(200);
-      this.sprites[this.currentId].play("right");
-      this.keyLock = true;
+    if (this.wKey.isDown) {
+      this.sprites[this.currentId].setVelocityY(-500);
+      if (
+        !this.sprites[this.currentId].anims.isPlaying ||
+        this.sprites[this.currentId].anims.currentAnim.key != "up"
+      ) {
+        this.sprites[this.currentId].play("up");
+      }
+    } else if (this.aKey.isDown) {
+      this.sprites[this.currentId].setVelocityX(-500);
+      if (
+        !this.sprites[this.currentId].anims.isPlaying ||
+        this.sprites[this.currentId].anims.currentAnim.key != "left"
+      ) {
+        this.sprites[this.currentId].play("left");
+      }
+    } else if (this.sKey.isDown) {
+      this.sprites[this.currentId].setVelocityY(500);
+      if (
+        !this.sprites[this.currentId].anims.isPlaying ||
+        this.sprites[this.currentId].anims.currentAnim.key != "down"
+      ) {
+        this.sprites[this.currentId].play("down");
+      }
+    } else if (this.dKey.isDown) {
+      this.sprites[this.currentId].setVelocityX(500);
+      if (
+        !this.sprites[this.currentId].anims.isPlaying ||
+        this.sprites[this.currentId].anims.currentAnim.key != "right"
+      ) {
+        this.sprites[this.currentId].play("right");
+      }
     }
     this.drawState();
   }
@@ -114,11 +209,10 @@ class MainScene extends Phaser.Scene {
         });
         const url = URL.createObjectURL(svgBlob);
         this.load
-          .spritesheet(
-            "player" + userId,
-            `${process.env.PUBLIC_URL}/player.png`,
-            { frameWidth: 32, frameHeight: 32 }
-          )
+          .spritesheet("player" + userId, playerSprite, {
+            frameWidth: 192,
+            frameHeight: 264,
+          })
           .on(
             "filecomplete",
             () => {
@@ -126,8 +220,6 @@ class MainScene extends Phaser.Scene {
                 this.sprites[userId].loading = false;
                 setTimeout(() => {
                   //had to add this delay for images to always show
-
-                  console.log(this.sprites[this.currentId].x);
                   this.sprites[userId] = this.physics.add
                     .sprite(
                       this.state[userId].x,
@@ -136,51 +228,50 @@ class MainScene extends Phaser.Scene {
                     )
                     .setScale(1, 1)
                     .setOrigin(0, 0);
-                  this.physics.add.collider(
-                    this.sprites[userId],
-                    this.wallLayer
-                  );
-                  this.physics.add.collider(
-                    this.sprites[userId],
-                    this.treeLayer
-                  );
+
                   this.anims.create({
                     key: "down",
-                    frameRate: 5,
+                    frameRate: 30,
                     frames: this.anims.generateFrameNumbers("player" + userId, {
-                      start: 0,
-                      end: 3,
+                      start: 15,
+                      end: 29,
                     }),
                     repeat: 0,
                   });
                   this.anims.create({
                     key: "left",
-                    frameRate: 5,
+                    frameRate: 30,
                     frames: this.anims.generateFrameNumbers("player" + userId, {
-                      start: 4,
-                      end: 7,
+                      start: 30,
+                      end: 44,
                     }),
                     repeat: 0,
                   });
                   this.anims.create({
                     key: "up",
-                    frameRate: 5,
+                    frameRate: 30,
                     frames: this.anims.generateFrameNumbers("player" + userId, {
-                      start: 8,
-                      end: 11,
+                      start: 0,
+                      end: 14,
                     }),
                     repeat: 0,
                   });
                   this.anims.create({
                     key: "right",
-                    frameRate: 5,
+                    frameRate: 30,
                     frames: this.anims.generateFrameNumbers("player" + userId, {
-                      start: 12,
-                      end: 15,
+                      start: 45,
+                      end: 59,
                     }),
                     repeat: 0,
                   });
-                }, 100);
+                  this.sprites[userId].play("down");
+
+                  this.physics.add.collider(
+                    this.sprites[userId],
+                    this.wallLayer
+                  );
+                }, 200);
               }
             },
             this
@@ -192,6 +283,7 @@ class MainScene extends Phaser.Scene {
           if (d > 1)
             if (this.sprites[userId].y < this.state[userId].y) {
               this.sprites[userId].y += d;
+
               this.sprites[userId].play("down");
             } else if (this.sprites[userId].y > this.state[userId].y) {
               this.sprites[userId].y -= d;
@@ -208,9 +300,9 @@ class MainScene extends Phaser.Scene {
             }
         } else {
           this.cameras.main.startFollow(this.sprites[this.currentId], true);
-          if (Math.abs(this.sprites[userId].x - this.state[userId].x) > 500)
+          if (Math.abs(this.sprites[userId].x - this.state[userId].x) > 3500)
             this.sprites[userId].x = this.state[userId].x;
-          if (Math.abs(this.sprites[userId].y - this.state[userId].y) > 500)
+          if (Math.abs(this.sprites[userId].y - this.state[userId].y) > 3500)
             this.sprites[userId].y = this.state[userId].y;
         }
       }
